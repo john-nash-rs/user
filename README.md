f7da3db3f39e14bdd1b79489f17860e9c922cd4d
HV <harshvardhanpe1@gmail.com>
2023-05-28 21:50:41 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Example
+
+POST
+
+curl -X POST -H "Content-Type: application/json" -d '{
+"name": "John Doe",
+"email": "johndoe@example.com"
+}' http://localhost:8080/users
+
+GET
+
+curl -X GET http://localhost:8080/users/1
+
+PUT
+
+curl -X PUT -H "Content-Type: application/json" -d 'abc@gmail.com' "http://localhost:8080/users/1/email"  
+
+DELETE
+
+curl -X DELETE http://localhost:8080/users/1
+
---
+++ src/main/java/app/skillcaptain/user/UserController.java
@@ -0,0 +1,60 @@
+package app.skillcaptain.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        if (optionalUser.isPresent()) {
+            return ResponseEntity.ok(optionalUser.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}/email")
+    public ResponseEntity<User> updateUserEmail(@PathVariable Long id, @RequestBody String email) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setEmail(email);
+            userRepository.save(user);
+
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        if (optionalUser.isPresent()) {
+            userRepository.deleteById(id);
+            return ResponseEntity.ok("User deleted successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userRepository.save(user);
+        return ResponseEntity.ok(createdUser);
+    }
+}
+
